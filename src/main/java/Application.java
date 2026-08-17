c3598eddb15ee2558d6dd75f4d2e32d5778bd169
yihyun-kwon <yihyun123@gmail.com>
2026-08-17 19:56:14 +0900

    Update Category enum

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,11 @@
+import com.ohgiraffers.perfmanagementsystem.model.Category;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
     }
 }
--- src/main/java/com/ohgiraffers/perfmanagementsystem/model/Category.java
+++ src/main/java/com/ohgiraffers/perfmanagementsystem/model/Category.java
@@ -1,10 +1,24 @@
 package com.ohgiraffers.perfmanagementsystem.model;
 
+/*
+* 공연의 종류(장르)를 표현하는 enum이다.
+* 화면에 출력될 한글 이름(description)을 final로 지정하여 변경되지 않도록 설정 한다. */
+
 public enum Category {
-     MUSICAL,           // 뮤지컬
-    CONCERT,            // 콘서트
-    PLAY,               // 연극
-    CLASSICAL_DANCE,    // 클래식-댄스
-    KIDS_FAMILY,        // 아동-가족
-    EXHIBITION          // 전시
+     MUSICAL("뮤지컬"),           // 뮤지컬
+    CONCERT("콘서트"),            // 콘서트
+    PLAY("연극"),               // 연극
+    CLASSICAL_DANCE("클래식·댄스"),    // 클래식·댄스
+    KIDS_FAMILY("아동·가족"),        // 아동·가족
+    EXHIBITION("전시");          // 전시
+
+    private final String description;
+
+    Category(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
